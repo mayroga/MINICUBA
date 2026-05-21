b22ad5f4448035c69d58a64a7bbb0f36fceb3b36
mayroga <132472344+mayroga@users.noreply.github.com>
2026-05-21 09:33:39 -0700

    Delete frontend directory

--- frontend/storage.js
+++
@@ -1,6 +0,0 @@
-// Solo respaldo local
-function organizarLocal(){
-  let base = JSON.parse(localStorage.getItem("MINICUBA_FILES")) || [];
-  base.sort((a,b)=>a.nombre.localeCompare(b.nombre));
-  localStorage.setItem("MINICUBA_FILES",JSON.stringify(base));
-}
